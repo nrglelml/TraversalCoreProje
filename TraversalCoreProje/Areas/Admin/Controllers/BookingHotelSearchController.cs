1013556cc6eb2c118ec06cf620f12b79ab392471
Nurgul Elmali <101966469+nrglelml@users.noreply.github.com>
2026-06-19 15:16:38 +0300

    booking_api_hotel_list_added_to_project

--- TraversalCoreProje/Areas/Admin/Controllers/BookingHotelSearchController.cs
+++ TraversalCoreProje/Areas/Admin/Controllers/BookingHotelSearchController.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using TraversalCoreProje.Areas.Admin.Models;
 
 namespace TraversalCoreProje.Areas.Admin.Controllers
 {
@@ -7,9 +9,81 @@ namespace TraversalCoreProje.Areas.Admin.Controllers
     [AllowAnonymous]
     public class BookingHotelSearchController : Controller
     {
+        private readonly string apiKey = "f6e6f3b192mshd8d9c354404e70fp1f402ejsn3a919764ef3b";
+        private readonly string apiHost = "booking-com15.p.rapidapi.com";
+
+        [HttpGet]
         public IActionResult Index()
         {
             return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Index(string city)
+        {
+            if (string.IsNullOrEmpty(city))
+            {
+                return View();
+            }
+
+            var destinations = await SearchDestination(city);
+
+            if (destinations == null || destinations.Count == 0)
+            {
+                ViewBag.Message = "Şehir bulunamadı.";
+                return View();
+            }
+
+            var firstMatch = destinations[0];
+            return RedirectToAction("HotelList", new { dest_id = firstMatch.dest_id, cityName = firstMatch.name });
+        }
+
+        public async Task<IActionResult> HotelList(string dest_id, string cityName)
+        {
+            var client = new HttpClient();
+            var request = new HttpRequestMessage
+            {
+                Method = HttpMethod.Get,
+                RequestUri = new Uri($"https://{apiHost}/api/v1/hotels/searchHotels?dest_id={dest_id}&search_type=CITY&arrival_date=2026-07-01&departure_date=2026-08-01&adults=2&children_age=0%2C17&room_qty=1&page_number=1&units=metric&temperature_unit=c&languagecode=tr&currency_code=TRY"),
+                Headers =
+                {
+                    { "x-rapidapi-key", apiKey },
+                    { "x-rapidapi-host", apiHost },
+                },
+            };
+
+            using (var response = await client.SendAsync(request))
+            {
+                response.EnsureSuccessStatusCode();
+                var body = await response.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<BookingHotelSearchViewModel.Rootobject>(body);
+
+                ViewBag.CityName = cityName;
+                return View(values?.data?.hotels ?? new BookingHotelSearchViewModel.Hotel[0]);
+            }
+        }
+        // ---------------- HELPER ----------------
+        private async Task<List<BookingDestinationSearchViewModel.Datum>> SearchDestination(string city)
+        {
+            var client = new HttpClient();
+            var request = new HttpRequestMessage
+            {
+                Method = HttpMethod.Get,
+                RequestUri = new Uri($"https://{apiHost}/api/v1/hotels/searchDestination?query={city}"),
+                Headers =
+                {
+                    { "x-rapidapi-key", apiKey },
+                    { "x-rapidapi-host", apiHost },
+                },
+            };
+
+            using (var response = await client.SendAsync(request))
+            {
+                response.EnsureSuccessStatusCode();
+                var body = await response.Content.ReadAsStringAsync();
+                var result = JsonConvert.DeserializeObject<BookingDestinationSearchViewModel>(body);
+                return result?.data;
+            }
         }
     }
 }
---
+++ TraversalCoreProje/Areas/Admin/Models/BookingHotelSearchViewModel.cs
@@ -0,0 +1,202 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TraversalCoreProje.Areas.Admin.Models
+{
+    public class BookingHotelSearchViewModel
+    {
+
+        public class Rootobject
+        {
+            public bool status { get; set; }
+            public string message { get; set; }
+            public long timestamp { get; set; }
+            public Data data { get; set; }
+        }
+
+        public class Data
+        {
+            public Hotel[] hotels { get; set; }
+            public Meta[] meta { get; set; }
+            public Appear[] appear { get; set; }
+        }
+
+        public class Hotel
+        {
+            public int hotel_id { get; set; }
+            public string accessibilityLabel { get; set; }
+            public Property1 property { get; set; }
+        }
+
+        public class Property1
+        {
+            public int optOutFromGalleryChanges { get; set; }
+            public Checkin checkin { get; set; }
+            public int rankingPosition { get; set; }
+            public int qualityClass { get; set; }
+            public float reviewScore { get; set; }
+            public int id { get; set; }
+            public int mainPhotoId { get; set; }
+            public int propertyClass { get; set; }
+            public string name { get; set; }
+            public string wishlistName { get; set; }
+            public int position { get; set; }
+            public float latitude { get; set; }
+            public string countryCode { get; set; }
+            public bool isFirstPage { get; set; }
+            public string[] photoUrls { get; set; }
+            public int reviewCount { get; set; }
+            public string checkinDate { get; set; }
+            public string checkoutDate { get; set; }
+            public string[] blockIds { get; set; }
+            public string currency { get; set; }
+            public float longitude { get; set; }
+            public int ufi { get; set; }
+            public string reviewScoreWord { get; set; }
+            public Pricebreakdown priceBreakdown { get; set; }
+            public Checkout checkout { get; set; }
+            public int accuratePropertyClass { get; set; }
+            public bool isPreferred { get; set; }
+        }
+
+        public class Checkin
+        {
+            public string untilTime { get; set; }
+            public string fromTime { get; set; }
+        }
+
+        public class Pricebreakdown
+        {
+            public Grossprice grossPrice { get; set; }
+            public Strikethroughprice strikethroughPrice { get; set; }
+            public Benefitbadge[] benefitBadges { get; set; }
+            public Excludedprice excludedPrice { get; set; }
+            public object[] taxExceptions { get; set; }
+        }
+
+        public class Grossprice
+        {
+            public float value { get; set; }
+            public string currency { get; set; }
+        }
+
+        public class Strikethroughprice
+        {
+            public float value { get; set; }
+            public string currency { get; set; }
+        }
+
+        public class Excludedprice
+        {
+            public float value { get; set; }
+            public string currency { get; set; }
+        }
+
+        public class Benefitbadge
+        {
+            public string variant { get; set; }
+            public string text { get; set; }
+            public string explanation { get; set; }
+            public string identifier { get; set; }
+        }
+
+        public class Checkout
+        {
+            public string untilTime { get; set; }
+            public string fromTime { get; set; }
+        }
+
+        public class Meta
+        {
+            public string title { get; set; }
+        }
+
+        public class Appear
+        {
+            public string id { get; set; }
+            public Component component { get; set; }
+            public string contentUrl { get; set; }
+        }
+
+        public class Component
+        {
+            public Props props { get; set; }
+        }
+
+        public class Props
+        {
+            public Content content { get; set; }
+            public bool fill { get; set; }
+            public string title { get; set; }
+            public string text { get; set; }
+        }
+
+        public class Content
+        {
+            public Props1 props { get; set; }
+        }
+
+        public class Props1
+        {
+            public Item[] items { get; set; }
+            public bool fitContentWidth { get; set; }
+        }
+
+        public class Item
+        {
+            public Props2 props { get; set; }
+        }
+
+        public class Props2
+        {
+            public Component1 component { get; set; }
+        }
+
+        public class Component1
+        {
+            public Props3 props { get; set; }
+        }
+
+        public class Props3
+        {
+            public string spacing { get; set; }
+            public Item1[] items { get; set; }
+            public string accessibilityLabel { get; set; }
+            public string icon { get; set; }
+            public string variant { get; set; }
+            public string tertiaryTintedColor { get; set; }
+        }
+
+        public class Item1
+        {
+            public Props4 props { get; set; }
+        }
+
+        public class Props4
+        {
+            public Text[] text { get; set; }
+        }
+
+        public class Text
+        {
+            public string font { get; set; }
+            public string text { get; set; }
+            public string color { get; set; }
+            public Linkaction[] linkActions { get; set; }
+        }
+
+        public class Linkaction
+        {
+            public Props5 props { get; set; }
+        }
+
+        public class Props5
+        {
+            public string url { get; set; }
+        }
+
+
+    }
+}
