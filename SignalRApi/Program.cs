04f235cf2a324a6582407508d48bea3d44e866a8
Nurgul Elmali <101966469+nrglelml@users.noreply.github.com>
2026-06-21 20:37:05 +0300

    added_signalR_to_project

---
+++ SignalRApi/Program.cs
@@ -0,0 +1,38 @@
+using Microsoft.EntityFrameworkCore;
+using SignalRApi.DAL;
+using SignalRApi.Hubs;
+using SignalRApi.Model;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddEntityFrameworkNpgsql().AddDbContext<Context>(options =>
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddSignalR();
+builder.Services.AddScoped<VisitorService>();
+builder.Services.AddCors(options => options.AddPolicy("CorsPolicy",
+    build =>
+    {
+        build.AllowAnyHeader().AllowAnyMethod().SetIsOriginAllowed((host => true)).AllowCredentials();
+    }));
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseAuthorization();
+
+
+app.MapControllers();
+app.MapHub<VisitorHub>("/VisitorHub");
+app.Run();
